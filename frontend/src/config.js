61967e0e5a5e027bb0b24e5a37539f80c8b229ac
vinayak <fobo401@gmail.com>
2023-10-14 17:35:48 +0530

    domain

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'https://vercel.com/vinayak-0017/notesy-api/AUaRDryXvLjwnNKwonZedUXDTYAp';
-export const BASE_URL = 'https://localhost:3000';
+export const BASE_URL = 'https://vercel.com/vinayak-0017/notesy-api/AUaRDryXvLjwnNKwonZedUXDTYAp';
+// export const BASE_URL = 'https://localhost:3000';
 // 
