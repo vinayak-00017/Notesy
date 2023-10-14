c8d41f9bb50b1a52c2e31e9698b45b7a3ef1ee6f
vinayak <fobo401@gmail.com>
2023-10-14 17:11:10 +0530

    hotfix

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ mongoose.connect(`${process.env.MONGO_URL}`,
                 {dbName : `${process.env.MONGO_DB}`}
 )
 
-// app.listen(3000, () => console.log('Server is listening on port 3000'))
+app.listen(3000, () => console.log('Server is listening on port 3000'))
 
 
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'https://vercel.com/vinayak-0017/notesy-api/AUaRDryXvLjwnNKwonZedUXDTYAp';
+// export const BASE_URL = 'https://vercel.com/vinayak-0017/notesy-api/AUaRDryXvLjwnNKwonZedUXDTYAp';
+export const BASE_URL = 'https://localhost:3000';
 // 
